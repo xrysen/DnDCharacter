761c315e757f19bbe86cfd8060f57d16bd343303
Sean Oyler <xrysen@gmail.com>
2021-04-22 15:00:48 -0700

    Adds route to classes by name

--- server/server.js
+++ server/server.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 8000 || process.env.PORT;
 
+const { races, subRaces } = require("./data/races");
+
+app.use(express.json());
+
+app.get("/races", (req, res) => {
+  if (!req.query.name) {
+    res.json(races);
+  } else {
+    for (const race of races) {
+      if (race.name.toLowerCase() === req.query.name.toLowerCase()) {
+        res.json(race);
+      }
+    }
+  }
+})
+
+app.get("/races/:id", (req, res) => {
+  res.json(races[req.params.id - 1])
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
